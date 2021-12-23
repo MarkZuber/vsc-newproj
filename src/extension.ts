bef84c816d9d3e1811c7cfd28f638a8d3d9751f1
Mark Zuber <mark@zube.com>
2021-12-23 14:36:53 -0800

    Progress on project creation

--- src/extension.ts
+++ src/extension.ts
@@ -1,13 +1,63 @@
 import * as vscode from "vscode";
+import { ProjectCreator, ProjectLanguage } from "./projectCreator";
 
 // called when extension is activated
-export function activate(context: vscode.ExtensionContext) {
+export async function activate(context: vscode.ExtensionContext) {
   console.log('Congratulations, your extension "vsc-newproj" is now active!');
 
   let disposable = vscode.commands.registerCommand(
     "vsc-newproj.createNewProject",
-    () => {
+    async () => {
       vscode.window.showInformationMessage("Creating new project...!");
+
+      const creator = new ProjectCreator();
+      const selectedLanguage = await vscode.window.showQuickPick(
+        creator.getProjectLanguages().map((x) => x.getName()),
+        { title: "Select the Language for your new project" }
+      );
+      if (selectedLanguage) {
+        const projectLanguage =
+          creator.getProjectLanguageByName(selectedLanguage);
+
+        if (projectLanguage) {
+          const selectedProjectDescription = await vscode.window.showQuickPick(
+            projectLanguage.getProjectTypes().map((x) => x.getShortDesc())
+          );
+          if (selectedProjectDescription) {
+            const projectType = projectLanguage.getProjectTypeByShortDesc(
+              selectedProjectDescription
+            );
+
+            if (projectType) {
+              const fileUri = await vscode.window.showOpenDialog({
+                title: "Pick a folder for the root of your project",
+                canSelectFiles: false,
+                canSelectFolders: true,
+                canSelectMany: false,
+              });
+
+              if (fileUri) {
+                const projectName = await vscode.window.showInputBox({
+                  title: "Enter Project Name (will be dir name)",
+                  placeHolder: "mynewproject",
+                });
+
+                const dirPath = fileUri[0].path;
+
+                if (projectName) {
+                  const commandRun = await projectType.createProject(
+                    dirPath,
+                    projectName
+                  );
+                  vscode.window.showInformationMessage(
+                    `We ran this command: ${commandRun}`
+                  );
+                }
+              }
+            }
+          }
+        }
+      }
     }
   );
 
---
+++ src/projectCreator.ts
@@ -0,0 +1,89 @@
+import * as path from "path";
+import * as vscode from "vscode";
+import { execa } from "execa";
+
+export abstract class ProjectType {
+  constructor(private shortDesc: string) {}
+
+  public getShortDesc(): string {
+    return this.shortDesc;
+  }
+
+  public abstract createProject(
+    directoryPath: string,
+    projectName: string
+  ): Promise<string>;
+}
+
+class RustProjectType extends ProjectType {
+  constructor(shortDesc: string) {
+    super(shortDesc);
+  }
+
+  public async createProject(
+    directoryPath: string,
+    projectName: string
+  ): Promise<string> {
+    const targetDir = path.join(directoryPath, projectName.replace(" ", ""));
+    const command = `cargo new --${this.getShortDesc()} ${targetDir}`;
+
+    try {
+      const { stdout, stderr } = await execa(command);
+      console.log("stdout: " + stdout);
+      console.log("stderr: " + stderr);
+
+      vscode.commands.executeCommand(
+        "vscode.openFolder",
+        vscode.Uri.parse(targetDir),
+        {
+          forceNewWindow: true,
+        }
+      );
+    } catch (error) {
+      console.log("error: " + error);
+    }
+
+    return command;
+  }
+}
+
+export class ProjectLanguage {
+  constructor(private name: string, private projectTypes: ProjectType[]) {}
+  public getName(): string {
+    return this.name;
+  }
+
+  public getProjectTypes(): ProjectType[] {
+    return this.projectTypes;
+  }
+
+  public getProjectTypeByShortDesc(shortDesc: string): ProjectType | undefined {
+    const filtered = this.projectTypes.filter(
+      (x) => x.getShortDesc() === shortDesc
+    );
+    return filtered.length === 1 ? filtered[0] : undefined;
+  }
+}
+
+export class ProjectCreator {
+  private languages = [
+    new ProjectLanguage("Rust", [
+      new RustProjectType("lib"),
+      new RustProjectType("bin"),
+    ]),
+  ];
+
+  public getProjectLanguages(): ProjectLanguage[] {
+    return this.languages;
+  }
+
+  public getProjectLanguageByName(
+    languageName: string
+  ): ProjectLanguage | undefined {
+    const filtered = this.languages.filter((x) => x.getName() === languageName);
+    if (filtered.length === 1) {
+      return filtered[0];
+    }
+    return undefined;
+  }
+}
