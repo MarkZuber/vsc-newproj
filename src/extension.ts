01a27b6e2039559cec3aac18a83c8a0e33971880
Mark Zuber <mark@zube.com>
2022-01-03 07:54:40 -0800

    Clean up control flow in extension ts

--- src/extension.ts
+++ src/extension.ts
@@ -2,7 +2,7 @@ import * as vscode from "vscode";
 import { ProjectCreator } from "./projectCreator";
 
 export async function activate(context: vscode.ExtensionContext) {
-  console.log('Congratulations, your extension "vsc-newproj" is now active!');
+  console.log('vsc-newproj activated');
 
   let disposable = vscode.commands.registerCommand(
     "vsc-newproj.createNewProject",
@@ -24,48 +24,68 @@ export async function activate(context: vscode.ExtensionContext) {
             .sort(),
           { title: "Select the Language for your new project" }
         ));
-      if (selectedLanguage) {
-        const projectLanguage =
-          creator.getProjectLanguageByName(selectedLanguage);
 
-        if (projectLanguage) {
-          const selectedProjectDescription = defaultProjectType || await vscode.window.showQuickPick(
-            projectLanguage.getProjectTypes().map((x) => x.getShortDesc()),
-            { title: `Select project type for your ${projectLanguage.getName()} project` }
-          );
-          if (selectedProjectDescription) {
-            const projectType = projectLanguage.getProjectTypeByShortDesc(
-              selectedProjectDescription
-            );
+      if (!selectedLanguage) {
+        return;
+      }
 
-            if (projectType) {
-              let dirPath = defaultProjectRootConfig;
-              if (!dirPath) {
-                const fileUri = await vscode.window.showOpenDialog({
-                  title: `Pick a root folder for your ${projectLanguage.getName()}/${projectType.getShortDesc()} project`,
-                  canSelectFiles: false,
-                  canSelectFolders: true,
-                  canSelectMany: false,
-                });
+      const projectLanguage =
+        creator.getProjectLanguageByName(selectedLanguage);
 
-                if (fileUri) {
-                  dirPath = fileUri[0].path;
-                }
-              }
+      if (!projectLanguage) {
+        vscode.window.showErrorMessage(`Invalid language in config: ${selectedLanguage}`);
+        return;
+      }
 
-              if (dirPath) {
-                const projectName = await vscode.window.showInputBox({
-                  title: `Enter Project/Dir Name for project lang/type: ${projectLanguage.getName()}/${projectType.getShortDesc()}`,
-                  placeHolder: "mynewproject",
-                });
+      const selectedProjectDescription = defaultProjectType || await vscode.window.showQuickPick(
+        projectLanguage.getProjectTypes().map((x) => x.getShortDesc()),
+        { title: `Select project type for your ${projectLanguage.getName()} project` }
+      );
+      if (!selectedProjectDescription) {
+        return;
+      }
 
-                if (projectName) {
-                  await projectType.createProject(dirPath, projectName);
-                }
-              }
-            }
-          }
+      const projectType = projectLanguage.getProjectTypeByShortDesc(
+        selectedProjectDescription
+      );
+
+      if (!projectType) {
+        vscode.window.showErrorMessage(`Invalid project type in config: ${selectedProjectDescription}`);
+        return;
+      }
+
+      let selectedRootFolder = false;
+      let dirPath = defaultProjectRootConfig;
+      if (!dirPath) {
+        const fileUri = await vscode.window.showOpenDialog({
+          title: `Pick a root folder for your ${projectLanguage.getName()}/${projectType.getShortDesc()} project`,
+          canSelectFiles: false,
+          canSelectFolders: true,
+          canSelectMany: false,
+        });
+
+        if (fileUri) {
+          selectedRootFolder = true;
+          dirPath = fileUri[0].path;
         }
+      }
+
+      if (!dirPath) {
+        if (!selectedRootFolder) {
+          vscode.window.showErrorMessage(`Invalid dirPath in config: ${dirPath}`);
+        }
+        return;
+      }
+
+      const projectName = await vscode.window.showInputBox({
+        title: `Enter Project/Dir Name for project lang/type: ${projectLanguage.getName()}/${projectType.getShortDesc()}`,
+        placeHolder: "mynewproject",
+      });
+
+      if (projectName) {
+        await projectType.createProject(dirPath, projectName);
+      } else {
+        vscode.window.showInformationMessage('Project Name not input, cancelling.');
       }
     }
   );
